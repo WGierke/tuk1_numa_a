182edf4dad56efa643bc9e41b9af8a9762e52fb9
lawben <lawrencebenson95@gmail.com>
2017-06-15 15:38:22 +0200

    Add option to get multiple columns and benchmark them

--- src/main.cpp
+++ src/main.cpp
@@ -34,34 +34,86 @@ static void BM_ColumnScan_1GB_Sequential_Local(benchmark::State& state) {
 }
 BENCHMARK(BM_ColumnScan_1GB_Sequential_Local)->Unit(benchmark::kMillisecond);
 
-static void BM_ColumnScan_1GB_Sequential_Remote(benchmark::State& state) {
+// static void BM_ColumnScan_1GB_Sequential_Remote(benchmark::State& state) {
+//     Table table;
+
+//     auto column = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, numa_max_node());
+//     auto columnIndex = table.addColumn(column);
+
+//     SetAffinity(0);
+
+//     while (state.KeepRunning())
+//     {
+//         table.column(columnIndex)->scan();
+//     }
+// }
+// BENCHMARK(BM_ColumnScan_1GB_Sequential_Remote)->Unit(benchmark::kMillisecond);
+
+// static void BM_ColumnScan_1GB_Random_Local(benchmark::State& state) {
+//     Table table;
+
+//     auto column = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, 0);
+//     auto columnIndex = table.addColumn(column);
+
+//     SetAffinity(0);
+
+//     while (state.KeepRunning())
+//     {
+//         table.column(columnIndex)->scanRandom();
+//     }
+// }
+// BENCHMARK(BM_ColumnScan_1GB_Random_Local)->Unit(benchmark::kMillisecond);
+
+static void BM_ColumnScan_1GB_Sequential_Local_2_Columns(benchmark::State& state) {
     Table table;
 
-    auto column = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, numa_max_node());
-    auto columnIndex = table.addColumn(column);
+    auto column1 = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, 0);
+    auto columnIndex1 = table.addColumn(column1);
+
+    auto column2 = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, 0);
+    auto columnIndex2 = table.addColumn(column2);
 
     SetAffinity(0);
 
+    std::vector<std::size_t> columnIndices = {columnIndex1, columnIndex2};
+    auto cols = table.getColumns(columnIndices);
+
     while (state.KeepRunning())
     {
-        table.column(columnIndex)->scan();
+        for (ColumnPtr &col : cols)
+        {
+            col->scan();
+        }
     }
 }
-BENCHMARK(BM_ColumnScan_1GB_Sequential_Remote)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_ColumnScan_1GB_Sequential_Local_2_Columns)->Unit(benchmark::kMillisecond);
 
-static void BM_ColumnScan_1GB_Random_Local(benchmark::State& state) {
+
+static void BM_ColumnScan_1GB_Sequential_Local_10_Columns(benchmark::State& state) {
     Table table;
 
-    auto column = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, 0);
-    auto columnIndex = table.addColumn(column);
+    std::vector<std::size_t> columnIndices;
+
+    for (uint i = 0; i < 10; ++i)
+    {
+        auto column = std::make_shared<Column<uint8_t>>(1 * 1000 * 1000 * 1000UL, 0);
+        auto columnIndex = table.addColumn(column);
+        columnIndices.push_back(columnIndex);
+    }
 
     SetAffinity(0);
 
+    auto cols = table.getColumns(columnIndices);
+
     while (state.KeepRunning())
     {
-        table.column(columnIndex)->scanRandom();
+        for (ColumnPtr &col : cols)
+        {
+            col->scan();
+        }
     }
 }
-BENCHMARK(BM_ColumnScan_1GB_Random_Local)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_ColumnScan_1GB_Sequential_Local_10_Columns)->Unit(benchmark::kMillisecond);
+
 
 BENCHMARK_MAIN();
--- src/table.h
+++ src/table.h
@@ -6,16 +6,19 @@
 
 #include "column.h"
 
+typedef std::shared_ptr<AbstractColumn> ColumnPtr;
+
 class Table {
 
 public:
 
     template <typename T>
     std::size_t addColumn(std::shared_ptr<Column<T>> &col);
-    std::shared_ptr<AbstractColumn> column(std::size_t columnIndex) { return m_columns[columnIndex]; }
+    ColumnPtr column(std::size_t columnIndex) { return m_columns[columnIndex]; }
+    std::vector<ColumnPtr> getColumns(std::vector<std::size_t> &columnIndcies);
 
 protected:
-    std::vector<std::shared_ptr<AbstractColumn>> m_columns;
+    std::vector<ColumnPtr> m_columns;
 
 };
 
@@ -23,4 +26,16 @@ template <typename T>
 std::size_t Table::addColumn(std::shared_ptr<Column<T>> &col) {
     m_columns.emplace_back(col);
     return m_columns.size() - 1;
+}
+
+std::vector<ColumnPtr> Table::getColumns(std::vector<std::size_t> &columnIndcies) {
+
+    std::vector<ColumnPtr> cols(columnIndcies.size());
+
+    for (uint i = 0; i < columnIndcies.size(); ++i) 
+    {
+        cols.at(i) = this->column(columnIndcies.at(i));
+    }
+
+    return cols;
 }
