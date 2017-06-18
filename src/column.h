260b97b152622cb5d0e94ec1d4d699b5ea2182d9
Robert Schmid <r.schmid@outlook.com>
2017-06-18 17:12:52 +0200

    Parameterize benchmark; remove random scan

--- src/column.h
+++ src/column.h
@@ -7,7 +7,6 @@
 class AbstractColumn {
 public:
     virtual void scan() const = 0;
-    virtual void scanRandom() const = 0;
 };
 
 template <typename T>
@@ -19,12 +18,25 @@ public:
     typedef NumaAlloc<T> allocator_t;
 
     void scan() const override;
-    void scanRandom() const override;
+
     std::vector<T, allocator_t> &data() { return m_attributeVector; }
 
 protected:
     std::vector<T, allocator_t> m_attributeVector;
 };
+
+// fast random number generator
+inline unsigned long fastrandom(unsigned long &x, unsigned long &y, unsigned long &z) {
+    x ^= x << 16;
+    x ^= x >> 5;
+    x ^= x << 1;
+
+    unsigned long t = x;
+    x = y;
+    y = z;
+    z = t ^ x ^ y;
+    return z;
+}
 
 template<typename T>
 void Column<T>::scan() const {
@@ -32,16 +44,6 @@ void Column<T>::scan() const {
     for (auto &attribute : m_attributeVector)
     {
         aggregate ^= attribute;
-    }
-    std::cout << aggregate;
-}
-
-template<typename T>
-void Column<T>::scanRandom() const {
-    T aggregate = 0;
-    for (auto i = 0ul; i < m_attributeVector.size(); ++i)
-    {
-        aggregate ^= m_attributeVector[std::rand() % m_attributeVector.size()];
     }
     std::cout << aggregate;
 }
