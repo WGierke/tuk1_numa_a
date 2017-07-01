6db72d5c13b414b55270295c63d7678ffcfa3f49
Robert Schmid <r.schmid@outlook.com>
2017-07-01 21:36:05 +0000

    Use dense hash map from Google Sparsehash library in hash join

--- src/numaalloc.h
+++ src/numaalloc.h
@@ -11,13 +11,25 @@
 template <class T> class NumaAlloc {
 public:
 const int node;
-typedef T              value_type;
+
+typedef T         value_type;
+typedef size_t    size_type;
+typedef ptrdiff_t difference_type;
+typedef T*        pointer;
+typedef const T*  const_pointer;
+typedef T&        reference;
+typedef const T&  const_reference;
+
+template< class U > struct rebind { typedef NumaAlloc<U> other; };
+
 typedef std::true_type propagate_on_container_move_assignment;
 typedef std::true_type is_always_equal;
 
 NumaAlloc(int node = 0) noexcept : node{node} {};
 template <typename TOther>
 NumaAlloc(const NumaAlloc<TOther> &other) noexcept : node{other.node} {};
+
+size_type max_size() const { return size_t(-1); }
 
 T* allocate (size_t num) {
     auto ret = numa_alloc_onnode(num * sizeof(T), node);
